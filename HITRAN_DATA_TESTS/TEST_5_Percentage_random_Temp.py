7eac1de7d91faaa35009705c14356677372c3db9
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-10-06 11:28:23 +0200

    added text to what is happening, made the print statement better

--- HITRAN_DATA_TESTS/TEST_5_Percentage_random_Temp.py
+++ HITRAN_DATA_TESTS/TEST_5_Percentage_random_Temp.py
@@ -22,20 +22,24 @@ for wavenumber in list_of_wavenumbers:
     current_wavelength = wavenumber_to_wavelength(wavenumber)
     REAL_humidity = np.random.randint(0,100) # %
 
+    # getting the possible absorption coefficients of the air at this temperature T
     low_humidity, high_humidity = absorption_coefficient_air_composition_low_high_humidity_at_T(
         mu_nitrogen[wavenumber], mu_oxygen[wavenumber], mu_carbon_mono_oxide[wavenumber], mu_water[wavenumber], T)
 
+    # converting the possible absorption coefficients into their respective intensities
     low_intensity = measure_intensity(start_laser_intensity, low_humidity, path_length_laser)
     high_intensity = measure_intensity(start_laser_intensity, high_humidity, path_length_laser)
 
+    # from this we get our random 'measured' intensity
     random_intensity = ((high_intensity-low_intensity)*REAL_humidity/100) + low_intensity
 
+    # from which we can calculate our way back to a percentage
     humidity = find_humidity_from_intensity_at_T(start_laser_intensity, random_intensity, path_length_laser,
                                             mu_nitrogen[wavenumber], mu_oxygen[wavenumber],
                                             mu_carbon_mono_oxide[wavenumber], mu_water[wavenumber], T
                                             )
 
     print(f"\n"
-          f"for wavelength: {current_wavelength * 1e9:.2f} nm at T = {T}:\n"
+          f"for wavelength: {current_wavelength * 1e9:.2f} nm at T = {T} Celsius:\n"
           f"The real humidity is: {REAL_humidity} %\n"
           f"Calculated humidity is: {humidity} %\n")
