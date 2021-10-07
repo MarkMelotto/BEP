fe7b399dee70c33c608cdb822ff07c7c7b692307
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-10-07 10:01:59 +0200

    fixed some formatting

--- HITRAN_DATA_TESTS/TEST_1.py
+++ HITRAN_DATA_TESTS/TEST_1.py
@@ -9,7 +9,6 @@ mu_oxygen = get_mu_oxygen()
 
 mu_carbon_mono_oxide = get_mu_carbon_mono_oxide()
 
-
 # a list of wavenumbers to make it easier to look at all the data in the same loop
 list_of_wavenumbers = [5344, 7210, 7400, 8812, 10638, 10700]
 
@@ -23,10 +22,10 @@ for wavenumber in list_of_wavenumbers:
         absorption_coefficient_air_composition_low_high_humidity(mu_nitrogen[wavenumber], mu_oxygen[wavenumber],
                                                                  mu_carbon_mono_oxide[wavenumber], mu_water[wavenumber])
 
-    absorbed_light_low_humidity_intensity = measure_intensity(start_laser_intensity, low_humidity_absorption_coefficient,
-                                                    path_length_laser)
-    absorbed_light_high_humidity_intensity = measure_intensity(start_laser_intensity, high_humidity_absorption_coefficient,
-                                                     path_length_laser)
+    absorbed_light_low_humidity_intensity = measure_intensity(start_laser_intensity,
+                                                              low_humidity_absorption_coefficient, path_length_laser)
+    absorbed_light_high_humidity_intensity = measure_intensity(start_laser_intensity,
+                                                               high_humidity_absorption_coefficient, path_length_laser)
 
     intensity_low_dB = intensity_in_dB(start_laser_intensity, absorbed_light_low_humidity_intensity)
     intensity_high_dB = intensity_in_dB(start_laser_intensity, absorbed_light_high_humidity_intensity)
@@ -35,5 +34,5 @@ for wavenumber in list_of_wavenumbers:
           f"for wavelength: {current_wavelength * 1e9:.2f} nm:\n"
           f"low humidity: {absorbed_light_low_humidity_intensity:.4} Watt/cm2, {intensity_low_dB:.4} dB\n"
           f"high humidity: {absorbed_light_high_humidity_intensity:.4} Watt/cm2, {intensity_high_dB:.4} dB\n"
-          f"a difference of: {((absorbed_light_high_humidity_intensity - absorbed_light_low_humidity_intensity)/absorbed_light_low_humidity_intensity)*100:.4}% in intensity\n"     
-          f"and a change of: {intensity_low_dB-intensity_high_dB:.4} dB")
+          f"a difference of: {((absorbed_light_high_humidity_intensity - absorbed_light_low_humidity_intensity) / absorbed_light_low_humidity_intensity) * 100:.4}% in intensity\n"
+          f"and a change of: {intensity_low_dB - intensity_high_dB:.4} dB")
