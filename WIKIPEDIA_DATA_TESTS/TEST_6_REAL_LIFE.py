e1d4b2772c9bc20a2c75035d3f214507393b41f8
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-10-08 11:52:56 +0200

    added workable wavelengths, so a new set of data with removed wavelengths of 1.5 um and above

---
+++ WIKIPEDIA_DATA_TESTS/TEST_6_REAL_LIFE.py
@@ -0,0 +1,47 @@
+from functions.WIKI_functions import *
+from functions.WIKI_data import *
+from functions.functions import *
+import matplotlib.pyplot as plt
+
+absorption_coefficient_water = WIKI_get_workable_absorption_coeff_water()
+list_wavelengths = WIKI_workable_available_wavelengths()
+
+start_intensity = 0.100  # Watt/cm2
+path_length = 0.1  # meter
+
+# lists for the plot
+list_real_humidity = []
+list_calculated_humidity = []
+
+
+for wavelength in list_wavelengths:
+    REAL_humidity = np.random.randint(0, 100)  # %
+    T = np.random.randint(-20, 60)
+    list_real_humidity.append(REAL_humidity)  # for a plot
+
+    concentration_water = WIKI_absorption_coefficient_air_composition_high_humidity_at_T(absorption_coefficient_water[wavelength], T)
+
+    max_intensity_loss = measure_intensity(start_intensity, concentration_water, path_length)
+
+    random_intensity = ((max_intensity_loss - start_intensity) * REAL_humidity / 100) + start_intensity
+
+    humidity = WIKI_find_humidity_from_intensity_at_T(start_intensity, random_intensity, path_length,
+                                                 absorption_coefficient_water[wavelength], T)
+
+    list_calculated_humidity.append(humidity)
+
+    print(f"\n"
+          f"for wavelength: {wavelength * 1e9:.2f} nm over a path length of {path_length * 100} cm at T = {T}:\n"
+          f"The real humidity is: {REAL_humidity} %\n"
+          f"Calculated humidity is: {humidity} %\n")
+
+
+plt.plot(list_wavelengths, list_real_humidity, 'go', label='Real humidity')
+plt.plot(list_wavelengths, list_calculated_humidity, 'r.', label='Calculated humidity')
+plt.legend()
+plt.grid()
+plt.title('Humidity check at random T')
+plt.xlabel('Wavelength (m)')
+plt.ylim((0,100))
+plt.ylabel('Percentage Humidity')
+plt.show()
--- functions/WIKI_data.py
+++ functions/WIKI_data.py
@@ -5,3 +5,9 @@ def WIKI_get_absorption_coeff_water():
 
 def WIKI_available_wavelengths():
     return [718e-9, 810e-9, 940e-9, 1.15e-6, 1.38e-6, 1.88e-6, 2.68e-6, 6.3e-6]
+
+def WIKI_get_workable_absorption_coeff_water():
+    return {718e-9:1, 810e-9:1.5, 940e-9:20, 1.15e-6:100, 1.38e-6:30}
+
+def WIKI_workable_available_wavelengths():
+    return [718e-9, 810e-9, 940e-9, 1.15e-6, 1.38e-6]
