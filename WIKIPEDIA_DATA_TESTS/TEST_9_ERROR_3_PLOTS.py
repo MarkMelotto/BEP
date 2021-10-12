a768de6d1588d27deec84dac2c282412f4ffecc0
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-10-12 15:43:08 +0200

    tidying up

--- WIKIPEDIA_DATA_TESTS/TEST_9_ERROR_3_PLOTS.py
+++ WIKIPEDIA_DATA_TESTS/TEST_9_ERROR_3_PLOTS.py
@@ -6,11 +6,13 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
+# getting the required data
 absorption_coefficient_water = WIKI_get_workable_absorption_coeff_water()
 list_wavelengths = WIKI_workable_available_wavelengths()
+
+# choosing our wavelength
 wavelength = 940e-9
 
-random_temperature = True
 start_intensity = 0.100  # Watt/cm2
 path_length_list = make_2d_path_length_list(101, 101)  # meter
 path_length_list = path_length_list[:,:3]
@@ -21,6 +23,9 @@ decimal_round_list = make_2d_decimal_list(100)
 
 # number of iterations
 N = 10000
+
+# if we want to control the temperature, we do that here
+random_temperature = True
 if not random_temperature:
     T = 10
 
@@ -40,8 +45,7 @@ for i in range(len(path_length_list[:,0])):
 
             random_intensity = ((max_intensity_loss - start_intensity) * REAL_humidity / 100) + start_intensity
 
-            # to make this work as a real life situation, we cannot measure at 10 decimals
-            # print(decimal_round_list[0,j])
+            #measuring at different decimals (3,4,5)
             random_intensity = round(random_intensity, int(decimal_round_list[0,j]))
 
             humidity = WIKI_find_humidity_from_intensity_at_T(start_intensity, random_intensity, path_length_list[i,0],
@@ -49,20 +53,10 @@ for i in range(len(path_length_list[:,0])):
 
             temp_error_list.append(abs(humidity-REAL_humidity))
         error_list[i,j] = sum(temp_error_list)/len(temp_error_list)
-            # print(f"\n"
-            #       f"for wavelength: {wavelength * 1e9:.2f} nm over a path length of {path_length * 100} cm at T = {T}:\n"
-            #       f"The real humidity is: {REAL_humidity} %\n"
-            #       f"Calculated humidity is: {humidity} %\n")
 
 error_list = error_list[1:,:] # first row are nan
-# decimal_round_list = decimal_round_list[1:,:]
-# path_length_list = path_length_list[1:,:]
-print(error_list)
 
-# errors = error_list.flatten()
-
-
-print(np.array(error_list[:,0]).shape)
+# making the plots
 fig, axs = plt.subplots(3, 1, sharex='col')
 axs[0].plot(path_length_list[:-1,0], error_list[:-1,0])
 axs[0].set_title("Mean error % in the 1 mWatt range")
