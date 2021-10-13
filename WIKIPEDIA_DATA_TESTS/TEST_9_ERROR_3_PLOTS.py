527447ba4d666d200c7c390fa33589a0ab2e6e25
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-10-13 16:20:04 +0200

    changed the titles in the plot

--- WIKIPEDIA_DATA_TESTS/TEST_9_ERROR_3_PLOTS.py
+++ WIKIPEDIA_DATA_TESTS/TEST_9_ERROR_3_PLOTS.py
@@ -59,13 +59,13 @@ error_list = error_list[1:,:] # first row are nan
 # making the plots
 fig, axs = plt.subplots(3, 1, sharex='col')
 axs[0].plot(path_length_list[:-1,0], error_list[:,0])
-axs[0].set_title("Mean error % in the 1 mWatt range")
+axs[0].set_title(f"Mean error in the 1 mWatt range at N = {N}")
 
 axs[1].plot(path_length_list[:-1,0], error_list[:,1])
-axs[1].set_title("Mean error % in the .1 mWatt range")
+axs[1].set_title(f"Mean error in the .1 mWatt range at N = {N}")
 
 axs[2].plot(path_length_list[:-1,0], error_list[:,2])
-axs[2].set_title("Mean error % in the .01 mWatt range")
+axs[2].set_title(f"Mean error in the .01 mWatt range at N = {N}")
 
 for i in range(3):
     axs[i].grid()
