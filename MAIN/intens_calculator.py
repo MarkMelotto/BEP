967569f7dee992b6676edcae17db332ba09ed1d2
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-11-16 11:55:54 +0100

    fixed a discrepancy in the math, fixed now

--- MAIN/intens_calculator.py
+++ MAIN/intens_calculator.py
@@ -6,10 +6,9 @@ spectral_sensitivity = 0.9
 while True:
     volt = input("gimme the voltage measured in mV\n")
     volt = float(volt)  # mVolt
-    volt *= 1000  # Volt
+    volt *= 1e-3  # Volt
     current = volt/R  # Ampere
-
-    # current *= 1e6
+    current *= 1e6  # uA
     intensity = (current / 37) / spectral_sensitivity
 
     print(f"incoming intensity = {intensity} mW/cm2\n")
