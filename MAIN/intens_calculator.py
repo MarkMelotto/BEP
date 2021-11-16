71948d5f1962450eb3329c03c18f000a54c78153
Mark Melotto <M.Melotto@student.tudelft.nl>
2021-11-16 11:26:17 +0100

    added some comments

--- MAIN/intens_calculator.py
+++ MAIN/intens_calculator.py
@@ -5,9 +5,9 @@ spectral_sensitivity = 0.9
 
 while True:
     volt = input("gimme the voltage measured in mV\n")
-    volt = float(volt)
-    volt *= 1000
-    current = volt/R
+    volt = float(volt)  # mVolt
+    volt *= 1000  # Volt
+    current = volt/R  # Ampere
 
     # current *= 1e6
     intensity = (current / 37) / spectral_sensitivity
